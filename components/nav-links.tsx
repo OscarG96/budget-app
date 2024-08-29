a14f0f940b94c7d697aa0b464efeedbceb43c59b
Oscar <oscar.gtzv@hotmail.com>
2024-08-29 00:59:13 -0600

    added prisma orm

--- components/nav-links.tsx
+++ components/nav-links.tsx
@@ -28,13 +28,13 @@ export default function NavLinks() {
               key={link.name}
               href={link.href}
               className={clsx(
-                'flex h-[30px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white',
                 {
                   'bg-slate-400 text-blue-600': pathname === link.href,
                 },
               )}
             >
-              <LinkIcon className="w-6" />
+              
               <p className="hidden md:block">{link.name}</p>
             </Link>
           );
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,10 @@
+import Card from "../../components/card"
+
 export default function Dashboard() {
-    return <div>Dashboard</div>
+    return (
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <Card title="Spent" value={3}></Card>
+            <Card title="Saved" value={4}></Card>
+        </div>
+    )
 }
