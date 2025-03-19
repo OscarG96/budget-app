7eb563f63b6a7335fd25883dd6c0b9d6f3aa26fe
Oscar <oscar.gtzv@hotmail.com>
2025-03-19 13:30:33 -0600

    added default categories when user is created

--- src/pages/api/lib/repositories/categoriesRepo.ts
+++ src/pages/api/lib/repositories/categoriesRepo.ts
@@ -3,8 +3,20 @@ import { prisma } from "../prisma";
 
 export class CategoriesRepo {
   static async getCategories(user: User) {
-    return prisma.user.findUnique({ 
-      where: { email: user?.email ?? undefined }, 
-      select: {categories: true}})
+    return prisma.categories.findMany({ 
+      where: { authorId: user?.id ?? undefined }, 
+      })
+  }
+
+  static async setDefaultCategories(userId: number) {
+    return prisma.categories.createMany({
+      data: [
+        { name: "Food", authorId: userId },
+        { name: "Transport", authorId: userId },
+        { name: "Entertainment", authorId: userId },
+        { name: "Utilities", authorId: userId },
+        { name: "Health", authorId: userId },
+      ]
+    })
   }
 }
--- src/pages/api/lib/services/categoriesService.ts
+++ src/pages/api/lib/services/categoriesService.ts
@@ -5,4 +5,8 @@ export class CategoriesService {
   static async getUserCategories(user: User) {
     return CategoriesRepo.getCategories(user)
   }
+
+  static async setDefaultCategories(userId: number) {
+    return CategoriesRepo.setDefaultCategories(userId)
+  }
 }
